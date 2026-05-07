d751603e4fdae9d31b6b6462122b965d3d15e37e
shinobu <shinobu@WIN-C7HIUQ2TJAL>
2026-05-07 22:08:17 +0900

    0507

---
+++ src/chapter1/Chapter1_1.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Chapter1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Integer obj1 = Integer.valueOf(10);
+		int i1 = obj1.intValue();
+		
+		String s = "Java";
+		
+		System.out.println(s + obj1 + i1);
+
+	}
+
+}
