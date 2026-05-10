4aa9970cc6958a0e35a59c5e75d1d8ce6b7f09be
shinobu <shinobu@WIN-C7HIUQ2TJAL>
2026-05-10 21:38:00 +0900

    0510

--- src/chapter2/Section1_3.java
+++ src/chapter2/Section1_3.java
@@ -2,9 +2,25 @@ package chapter2;
 
 public class Section1_3 {
 
+	class Inner {
+		void print() {System.out.println("Inner");}
+	}
+	
+	static class Nested {
+		static void print() {System.out.println("Nested");}
+	}
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		Section1_3 outer = new Section1_3();
+		Inner inner1 = outer.new Inner();
+		Inner inner2 = new Section1_3().new Inner();
+		
+		Nested nested1 = new Section1_3.Nested();
+		Nested nested2 = new Nested();
 	}
 
 }
+
+public class 
